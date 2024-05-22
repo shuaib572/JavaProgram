cdfacf3f54cc29077ea268d540a299f3bf1154aa
Admin <Admin@DESKTOP-ML9U5I3.mshome.net>
2024-05-22 16:25:31 +0530

    Add eclipse

---
+++ src/Java8Program/DefaultMethod.java
@@ -0,0 +1,20 @@
+package Java8Program;
+
+interface Vehicle
+{
+	public void start();
+}
+public class DefaultMethod implements Vehicle{
+
+	@Override
+	public void start() {
+		System.out.println("Engine Start");
+		
+	}
+	public static void main(String[] args) {
+		
+		DefaultMethod ob=new DefaultMethod();
+		ob.start();
+	}
+
+}
