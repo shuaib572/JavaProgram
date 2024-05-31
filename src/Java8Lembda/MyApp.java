d6978177ad389f3d02071ef2cacdc85373d1cc99
Admin <Admin@DESKTOP-ML9U5I3.mshome.net>
2024-05-31 11:54:44 +0530

    Update

---
+++ src/Java8Lembda/MyApp.java
@@ -0,0 +1,20 @@
+package Java8Lembda;
+
+interface MyAppIn{
+	public void m1();
+}
+public class MyApp implements MyAppIn {
+
+	@Override
+	public void m1() {
+		System.out.println("M1 Method Called");
+		
+	}
+	public static void main(String[] args) {
+		MyApp ob=new MyApp();
+		ob.m1();
+		
+	}
+	
+
+}
--- src/Java8Lembda/MyAppOne.java
+++ src/Java8Lembda/MyAppOne.java
@@ -11,7 +11,7 @@ public class MyAppOne
 	
 	public static void main(String[] args) {
 		
-		 MyInterfaceOne mit=() -> System.out.println("m1 Methos called");
+		 MyInterfaceOne mit=() -> System.out.println("m1 Methos called"); // java 8
 		 mit.m1();
 	}
 	
