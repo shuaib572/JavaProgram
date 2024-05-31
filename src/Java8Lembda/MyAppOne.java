f806c337d525943da8bb8c17defff8e42973d511
Admin <Admin@DESKTOP-ML9U5I3.mshome.net>
2024-05-31 11:41:59 +0530

    update

--- src/Java8Lembda/MyAppOne.java
+++ src/Java8Lembda/MyAppOne.java
@@ -5,16 +5,14 @@ interface MyInterfaceOne
 	public void m1();
 }
 
-public class MyAppOne implements MyInterfaceOne{
+public class MyAppOne
+{
 
-	@Override
-	public void m1() {
-	System.out.println("My m1 Method Called");
-		
-	}
+	
 	public static void main(String[] args) {
-		MyAppOne ob=new MyAppOne();
-		ob.m1();
+		
+		 MyInterfaceOne mit=() -> System.out.println("m1 Methos called");
+		 mit.m1();
 	}
 	
 	
